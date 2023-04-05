a2fd7033539845e97c363a3f9543d6f8d28c4936
harshaanirudh <harsha.anirudh@gmail.com>
2023-04-04 18:45:53 -0700

    Changed booking Details

--- ppfrontend/src/Bookings/UserBookingsList.js
+++ ppfrontend/src/Bookings/UserBookingsList.js
@@ -1,11 +1,18 @@
 import {
+  Box,
   Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
   Collapse,
+  Grid,
   List,
+  ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
   ListSubheader,
+  Typography,
 } from "@mui/material";
 import { height } from "@mui/system";
 import React, { useEffect } from "react";
@@ -104,13 +111,73 @@ function UserBookingListItem(props) {
       <Collapse in={open} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary="Starred" />
+            <ListItem
+              sx={{
+                marginLeft: "5%",
+                width: "100%",
+              }}
+            >
+              <Grid container>
+                <Grid item xl={10} md={10}>
+                  <Card sx={{ display: "flex" }} elevation={5}>
+                    <CardHeader
+                      sx={{ width: "50%" }}
+                      title={`${booking.venue.venuename}`}
+                      subheader={`${booking.venue.address}, ${booking.venue.city}`}
+                    />
+                    <CardMedia
+                      component={"img"}
+                      width="200"
+                      // height={"200"}
+                      image={booking.venue.url[0].url}
+                    ></CardMedia>
+                    <CardContent sx={{ width: "100%" }}>
+                      <Typography component="div" variant="h5">{`${dayjs(
+                        booking.slots[0].timeslotstart
+                      ).format("dddd , YYYY MMMM DD")}`}</Typography>
+                      <Typography
+                        component="div"
+                        variant="h6"
+                      >{`Courts : ${booking.noofcourts}`}</Typography>
+                      <Slots slots={booking.slots}> </Slots>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Grid>
+            </ListItem>
           </ListItemButton>
         </List>
       </Collapse>
     </React.Fragment>
+  );
+}
+
+function Slots(props) {
+  let slots = props.slots;
+  console.log(slots);
+  return (
+    <div className="slots" style={{ display: "flex", flexWrap: "wrap" }}>
+      {slots.map((x) => {
+        return (
+          <Card
+            style={{ flexBasis: "70%", margin: "1%" }}
+            elevation={8}
+            key={x.id}
+          >
+            <CardContent
+              style={{
+                background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+              }}
+            >
+              <Typography color={"white"} component="p">
+                {`${dayjs(x.timeslotstart).format("hh:mm A")} - ${dayjs(
+                  x.timeslotend
+                ).format("hh:mm A")}`}
+              </Typography>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </div>
   );
 }
